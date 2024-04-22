4650170dd6ad38ae4c58fac945275fc8f17ccbd8
Bulelani Mashiane <166546475+BulelaniMashiane@users.noreply.github.com>
2024-04-22 12:02:13 +0200

    Add files via upload

---
+++ RegLogin/src/main/java/buli/reglogin/reglogin/loginTest.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package buli.reglogin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bulel
+ */
+public class loginTest {
+    
+    public loginTest() {
+    }
+
+    @Test
+    public void testCheckUserName() {
+         String userName = "Aus_1";
+        String password = "Aus1n*)99";
+    }
+
+    @Test
+    public void testCheckPasswordComplexity() {
+    }
+
+  
+    
+}
